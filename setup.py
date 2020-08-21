0c7313e7c4c9b6cc874a7072c826875f76bc9664
ERSSLE <253188236@qq.com>
2020-08-22 07:34:10 +0800

    test modify

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description = 'Ordinary Kriging Interpolation',
     long_description = long_description,
     long_description_content_type = 'text/markdown',
-    url = '',
+    url = 'https://github.com/ERSSLE/ordinary_kriging',
     packages = setuptools.find_packages(include=['kriging','kriging.*']),
     classifiers = [
         'Programming Language :: Python :: 3',
